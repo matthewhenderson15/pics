f064473d680be642118c85ca865cc59d348b92da
Matthew Henderson <info@matthewahenderson.com>
2022-08-16 21:30:49 -0400

    Added on form and on search submit functions to App and SearchBar components

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     state = { term: '' };
     
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        console.log(this.state.term);
+    }
+    
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input 
